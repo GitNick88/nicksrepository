63a73ec361012ad6e6e9072c0a1985aa468275e6
Nick Cole <tcnick12@gmail.com>
2020-02-10 18:42:23 -0500

    Commit number 3

--- app/tools.py
+++ app/tools.py
@@ -1,2 +1,25 @@
 def test():
     print('hello')
+
+
+# train/val/test split function
+def train_val_test_split(df):
+    train, test = train_test_split(df, test_size=0.2)
+    train, val = train_test_split(df, test_size=0.2)
+    print('Train:', train.shape)
+    print('Val:', val.shape)
+    print('Test:', test.shape)
+return train, val, test
+
+# function to check for sum of null values
+def null_sum(df):
+    sum_total = df.isnull().sum()
+    if sum_total.sum() == 0:
+      print('You have no nulls to worry about!')
+    if sum_total.sum() <= 200 and sum_total.sum() > 1:
+      print(f'You have some data cleaning to do!')
+    if sum_total.sum() >= 201:
+      print('This is one dirty dataset...')
+    print('Total nulls per column:\n', sum_total)
+
+null_sum(df)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="lambdata_nickcole",
-    version="0.1",
+    version="0.2",
     author="Nick Cole",
     author_email="tcnick12@gmail.com",
     description="Data helper functions package.",
